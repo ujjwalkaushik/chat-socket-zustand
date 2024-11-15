f6a342894285bfa25fb6af8c78427a7c07083f69
kush <ujjwalkaushik24@gmail.com>
2024-11-15 18:17:38 +0530

    Send and retrieve messages

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { TbSend } from "react-icons/tb";
+import useSendMessage from "../../hooks/useSendMessage";
 
 const MessageInput = () => {
+  const [message, setMessage] = useState("");
+
+  const {loading, sendMessage} = useSendMessage();
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    if(!message) return;
+    await sendMessage(message);
+    setMessage("");
+  }
+
   return (
-    <form className="px-4 my-3">
+    <form className="px-4 my-3" onSubmit={handleSubmit}>
       <div className="w-full relative">
         <input
           type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
           className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white"
           placeholder="Send a message"
         />
@@ -14,7 +28,7 @@ const MessageInput = () => {
           type="submit"
           className="absolute inset-y-0 end-0 flex items-center pe-3"
         >
-          <TbSend />
+          {loading ? <div className="loading loading-spinner"></div> : <TbSend />}
         </button>
       </div>
     </form>
--- frontend/src/components/messages/NoChatSelected.jsx
+++ frontend/src/components/messages/NoChatSelected.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { TbMessages } from "react-icons/tb";
+import { useAuthContext } from "../../context/AuthContext";
 
 const NoChatSelected = () => {
+  const { authUser: user } = useAuthContext();
+
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
-        <p>Welcome John Doe</p>
+        <p>Welcome {user.fullName}</p>
         <p> Select a chat to start messaging</p>
         <TbMessages className="text-3xl md:text-6xl text-center" />
       </div>
