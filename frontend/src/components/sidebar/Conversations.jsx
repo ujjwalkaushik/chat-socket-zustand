a812690a50d9366989cd7f80c1bf7f6d8731075f
kush <ujjwalkaushik24@gmail.com>
2024-11-17 21:05:58 +0530

    Socket Implementation

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -5,7 +5,6 @@ import { getRandomEmoji } from "../../utils/emojis";
 
 const Conversations = () => {
   const { loading, conversations } = useGetConversations();
-  console.log(conversations);
   return (
     <>
       <div className="py-2 flex flex-col overflow-auto">
