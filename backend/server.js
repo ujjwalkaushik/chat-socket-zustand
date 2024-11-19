cbf749b9a4ae93614b622dac19e2ba5554e35e58
kush <ujjwalkaushik24@gmail.com>
2024-11-19 17:11:57 +0530

    deploy ready

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
-import messageRoutes from "./routes/message.routes.js"
-import userRoutes from "./routes/user.routes.js"
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import connectToMongoDb from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 // const app = express();
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -22,7 +25,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-server.listen(PORT, ()=>  {
-    connectToMongoDb();
-    console.log(`server is running on PORT ${PORT}`);
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "fronten", "dist", "index.html"));
+});
+
+server.listen(PORT, () => {
+  connectToMongoDb();
+  console.log(`server is running on PORT ${PORT}`);
 });
